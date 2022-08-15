a9028823e93e7f69e024b29a001cca20f9b30314
BuiHuuDat <dat54261001@gmail.com>
2022-08-15 09:16:41 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,24 +73,6 @@ tabs.forEach(tab => {
     tab.classList.add('qualification__active')
   })
 })
-const modalViews = document.querySelectorAll('.services__modal'),
-  modalBtns = document.querySelectorAll('.services__button'),
-  modalCloses = document.querySelectorAll('.services__modal-close')
-let modal = function (modalClick) {
-  modalViews[modalClick].classList.add('active-modal')
-}
-modalBtns.forEach((modalBtn, i) => {
-  modalBtn.addEventListener('click', () => {
-    modal(i)
-  })
-})
-modalCloses.forEach((modalClose) => {
-  modalClose.addEventListener('click', () => {
-    modalViews.forEach((modalView) => {
-      modalView.classList.remove('active-modal')
-    })
-  })
-})
 
 let swiperQualification = new Swiper(".qualification__edu", {
   loop: true,
