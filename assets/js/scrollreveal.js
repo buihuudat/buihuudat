4a88d35b3785de1c0aa57c4f8c4a9fec17c3ac27
Bui Huu Dat <dat54261001@gmail.com>
2023-05-25 10:35:52 +0700

    update my work

--- assets/js/scrollreveal.js
+++ assets/js/scrollreveal.js
@@ -1,54 +1,67 @@
-const sr=ScrollReveal({distance:'60px',duration:2800,reset:0,})
-sr.reveal('.section__title',{origin:'top',})
-sr.reveal('.section__subtitle',{origin:'top',distance:'30px'})
-sr.reveal('.home__social a', {origin:'left', interval: 10, delay: 400})
-sr.reveal('.home__title', {origin:'top', delay: 100})
-sr.reveal('.home__subtitle', {origin:'top', delay: 150})
-sr.reveal('.home__description', {origin:'top', delay: 200})
-sr.reveal('.home__button', {origin:'top', delay: 250})
-sr.reveal('.home__img', {origin:'right', delay: 150})
-sr.reveal('.home__scroll', {origin:'top', delay: 350})
+const sr = ScrollReveal({ distance: "60px", duration: 2800, reset: 0 });
+sr.reveal(".section__title", { origin: "top" });
+sr.reveal(".section__subtitle", { origin: "top", distance: "30px" });
+sr.reveal(".home__social a", { origin: "left", interval: 10, delay: 400 });
+sr.reveal(".home__title", { origin: "top", delay: 100 });
+sr.reveal(".home__subtitle", { origin: "top", delay: 150 });
+sr.reveal(".home__description", { origin: "top", delay: 200 });
+sr.reveal(".home__button", { origin: "top", delay: 250 });
+sr.reveal(".home__img", { origin: "right", delay: 150 });
+sr.reveal(".home__scroll", { origin: "top", delay: 350 });
 // sr.reveal('.home__emoji .l', {origin:'right', delay: 450})
 // sr.reveal('.home__emoji .r', {origin:'top', delay: 350})
-sr.reveal('.about__imgs',{origin:'left',delay:150})
-sr.reveal('.about__title',{origin:'right',delay:150})
-sr.reveal('.about__description',{origin:'right',delay:250})
-sr.reveal('.about__buttons',{origin:'top',delay:300,distance:'30px'})
-sr.reveal('.skills-content-1',{origin:'left',delay:100})
-sr.reveal('.skills-content-2',{origin:'left',delay:200})
-sr.reveal('.skills-content-3',{origin:'right',delay:100})
-sr.reveal('.skills-content-4',{origin:'right',delay:200})
-sr.reveal('.ql-tt-1',{origin:'top',delay:100})
-sr.reveal('.ql-tt-2',{origin:'top',delay:200})
-sr.reveal('.qualification__rounder',{origin:'top',delay:100})
-sr.reveal('.qualification__line',{origin:'top',delay:100})
-sr.reveal('.qt-1',{origin:'left',delay:150})
-sr.reveal('.qst-1',{origin:'left',delay:200})
-sr.reveal('.qc-1',{origin:'top',delay:300,distance:'20px'})
-sr.reveal('.qt-2',{origin:'right',delay:200})
-sr.reveal('.qst-2',{origin:'right',delay:250})
-sr.reveal('.qc-2',{origin:'top',delay:350,distance:'20px'})
-sr.reveal('.qt-3',{origin:'left',delay:250})
-sr.reveal('.qst-3',{origin:'left',delay:300})
-sr.reveal('.qc-3',{origin:'top',delay:400,distance:'20px'})
-sr.reveal('.qt-4',{origin:'right',delay:300})
-sr.reveal('.qst-4',{origin:'right',delay:350})
-sr.reveal('.qc-4',{origin:'top',delay:450,distance:'40px'})
-sr.reveal('.qualification__edu',{origin:'bottom',})
+sr.reveal(".about__imgs", { origin: "left", delay: 150 });
+sr.reveal(".about__title", { origin: "right", delay: 150 });
+sr.reveal(".about__description", { origin: "right", delay: 250 });
+sr.reveal(".about__buttons", { origin: "top", delay: 300, distance: "30px" });
+sr.reveal(".skills-content-1", { origin: "left", delay: 100 });
+sr.reveal(".skills-content-2", { origin: "left", delay: 200 });
+sr.reveal(".skills-content-3", { origin: "right", delay: 100 });
+sr.reveal(".skills-content-4", { origin: "right", delay: 200 });
+sr.reveal(".ql-tt-1", { origin: "top", delay: 100 });
+sr.reveal(".ql-tt-2", { origin: "top", delay: 200 });
+// sr.reveal('.qualification__rounder',{origin:'top',delay:100})
+// sr.reveal('.qualification__line',{origin:'top',delay:100})
+sr.reveal(".qt-1", { origin: "left", delay: 150 });
+sr.reveal(".qst-1", { origin: "left", delay: 200 });
+sr.reveal(".qc-1", { origin: "top", delay: 300, distance: "20px" });
+sr.reveal(".qt-2", { origin: "right", delay: 200 });
+sr.reveal(".qst-2", { origin: "right", delay: 250 });
+sr.reveal(".qc-2", { origin: "top", delay: 350, distance: "20px" });
+sr.reveal(".qt-3", { origin: "left", delay: 250 });
+sr.reveal(".qst-3", { origin: "left", delay: 300 });
+sr.reveal(".qc-3", { origin: "top", delay: 400, distance: "20px" });
+sr.reveal(".qt-4", { origin: "right", delay: 300 });
+sr.reveal(".qst-4", { origin: "right", delay: 350 });
+sr.reveal(".qc-4", { origin: "top", delay: 450, distance: "40px" });
+sr.reveal(".qualification__edu", { origin: "bottom" });
 // sr.reveal('.services__content',{origin:'top',interval:100})
-sr.reveal('.services__icon',{origin:'top',interval:100})
-sr.reveal('.services__title',{origin:'top',interval:150})
-sr.reveal('.services__button',{origin:'left',interval:200})
-sr.reveal('.swiper-button-prev',{origin:'left',delay:100,distance:'20px'})
-sr.reveal('.swiper-button-next',{origin:'right',delay:100,distance:'20px'})
-sr.reveal('.swiper-pagination',{origin:'bottom',delay:100})
-sr.reveal('.project__container',{origin:'top',delay:100})
-sr.reveal('.testimonial__container',{origin:'top',delay:100})
-sr.reveal('.vl',{origin:'top',delay:100})
-sr.reveal('.contact__information',{origin:'top',interval:200,delay:10})
-sr.reveal('.contact__content',{origin:'top',interval:100,delay:100})
-sr.reveal('.contact__buttons',{origin:'top',delay:350,distance:'25px', delay:100})
-sr.reveal('.footer__title',{origin:'top',delay:100})
-sr.reveal('.footer__subtitle',{origin:'top',delay:200,distance:'30px'})
-sr.reveal('.soc a',{origin:'top',interval:100})
-sr.reveal('.footer__copy',{origin:'top',delay:100})
+sr.reveal(".services__icon", { origin: "top", interval: 100 });
+sr.reveal(".services__title", { origin: "top", interval: 150 });
+sr.reveal(".services__button", { origin: "left", interval: 200 });
+sr.reveal(".swiper-button-prev", {
+  origin: "left",
+  delay: 100,
+  distance: "20px",
+});
+sr.reveal(".swiper-button-next", {
+  origin: "right",
+  delay: 100,
+  distance: "20px",
+});
+sr.reveal(".swiper-pagination", { origin: "bottom", delay: 100 });
+sr.reveal(".project__container", { origin: "top", delay: 100 });
+sr.reveal(".testimonial__container", { origin: "top", delay: 100 });
+sr.reveal(".vl", { origin: "top", delay: 100 });
+sr.reveal(".contact__information", { origin: "top", interval: 200, delay: 10 });
+sr.reveal(".contact__content", { origin: "top", interval: 100, delay: 100 });
+sr.reveal(".contact__buttons", {
+  origin: "top",
+  delay: 350,
+  distance: "25px",
+  delay: 100,
+});
+sr.reveal(".footer__title", { origin: "top", delay: 100 });
+sr.reveal(".footer__subtitle", { origin: "top", delay: 200, distance: "30px" });
+sr.reveal(".soc a", { origin: "top", interval: 100 });
+sr.reveal(".footer__copy", { origin: "top", delay: 100 });
